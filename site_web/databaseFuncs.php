637ddab3771d5ebfd6bbd6d495d4dafb5187b9b7
n0m1s <n0m1s@homnomnom.fr>
2016-03-19 17:32:53 +0100

    added getUserId & addVote

--- site_web/databaseFuncs.php
+++ site_web/databaseFuncs.php
@@ -15,4 +15,38 @@ function connectDB()
     }
 }
 
+function getUserId($db, $username)
+{
+    try
+    {
+        $request = $db->prepare("SELECT ID FROM utilisateur WHERE Pseudo = ? LIMIT 1");
+        $request->execute(array($username));
+        $userId = $request->fetch();
+        $request->closeCursor();
+        return $userId;
+    }
+    catch (PDOException $e)
+    {
+        //NOTE: possible security problem with $e->errorMessage
+        die($e->getMessage());
+        echo "NOK";
+    }
+}
+
+function addVote($pdo, $userID, $vote, $offre)
+{
+    try
+    {
+        $pdo->beginTransaction();
+        $request = $db->prepare("INSERT INTO vote(Etat, Utilisateur, offre) VALUES(:vote, :user, :offre)");
+        $request->execute(array('vote'=>$vote, 'user'=>$userID, 'offre'=>$offre));
+        $pdo->commit();
+    }
+    catch (PDOException $e)
+    {
+        $pdo->rollBack();
+        die($e->errorMessage());
+    }
+}
+
 ?>
