9ad0d74cc618767ddabb699c7f671c1d2e36e9b3
n0m1s <n0m1s@homnomnom.fr>
2016-03-19 09:19:53 +0100

    agrandissement de la proposition de projet

--- site_web/projets.js
+++ site_web/projets.js
@@ -19,3 +19,31 @@ function retract_description_project(projId)
     moreButton.setAttribute("value", "more");
     moreButton.setAttribute("onClick", "extend_description_project("+projId+")");
 }
+
+function extend_proposition_project(projId, projPropId, projPropNumbers)
+{
+    document.getElementById("projPropActivationLink_"+projId+"_"+projPropId).setAttribute("onClick", "retract_proposition_project(" + projId + ", " + projPropId + ", " + projPropNumbers + ")");
+    for(var i = 0; i < projPropNumbers; ++i)
+    {
+        if(i == projPropId)
+            continue;
+        var other_project = document.getElementById("proj_"+projId+"_prop_"+i);
+        other_project.style.display = "none";
+    }
+    var propositionProject = document.getElementById("proj_"+projId+"_prop_"+projPropId);
+    propositionProject.style.width = "100%";
+}
+
+function retract_proposition_project(projId, projPropId, projPropNumbers)
+{
+    document.getElementById("projPropActivationLink_"+projId+"_"+projPropId).setAttribute("onClick", "extend_proposition_project(" + projId + ", " + projPropId + ", " + projPropNumbers + ")");
+    for(var i = 0; i < projPropNumbers; ++i)
+    {
+        if(i == projPropId)
+            continue;
+        var other_project = document.getElementById("proj_"+projId+"_prop_"+i);
+        other_project.style.display = "block";
+    }
+    var propositionProject = document.getElementById("proj_"+projId+"_prop_"+projPropId);
+    propositionProject.style.width = "auto";
+}
